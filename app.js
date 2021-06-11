b8609b2dea3f44133b35fd79d124c1a7545e0474
LazyCoder-1506 <pranayan1506@gmail.com>
2021-06-11 21:23:35 +0530

    async function

--- app.js
+++ app.js
@@ -2,30 +2,16 @@ function toggleCollapse(el) {
   $(el).next().collapse('toggle');
 }
 
-document.getElementById('get_btn').addEventListener('click', function(e) {
+document.getElementById('get_btn').addEventListener('click', async function(e) {
   e.preventDefault()
   const num = document.getElementById('number').value;
   let output = '<h2>Users</h2>'
   for (let i = 1; i <= num; i++) {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${i}`)
-    .then(post_response => post_response.json())
-    .then(post => {
-      console.log(post.title)
-      output += `
-      <div class="row mx-0 mb-5">
-        <div class="col-12">
-          <h5>${post.title}</h5>
-          <p class="mb-0">${post.body}</p>
-          <a role="button" class="view_comments mb-2" onclick="toggleCollapse(this)">View Comments</a>
-          <div class="collapse fade px-4">
-            <p class="mb-0">Comment Name</p>
-            <p class="mb-1 text-secondary">Email</p>
-            <p><small>Comment Body</small></p>
-          </div>
-        </div>
-      </div>`;
-      document.getElementById('container').innerHTML = output;
-    })
-  }
-  // document.getElementById('container').innerHTML = output;
+    let req = await fetch(`https://jsonplaceholder.typicode.com/posts/${i}`)
+    let post = await req.json()
+    output += `
+      <h5>${post.title}</h5>
+      <p>${post.body}</p>`
+    }
+  document.getElementById('container').innerHTML = output;
 })
